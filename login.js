c653cc52adc1e4ddaffb408ee63be2f6345fa4ed
해구 <165454268+heaguuu@users.noreply.github.com>
2025-07-30 07:52:14 +0900

    Update login.js

--- login.js
+++ login.js
@@ -8,8 +8,7 @@ document.getElementById("loginForm").addEventListener("submit", function (e) {
   if (id === "aqua" && pw === "1234") {
     document.getElementById("loginStatus").style.color = "green";
     document.getElementById("loginStatus").textContent = "로그인 성공!";
-    // 예: 메인 화면으로 이동 (main.html 파일이 필요함)
-    // window.location.href = "main.html";
+    window.location.href = "home.html";
   } else {
     document.getElementById("loginStatus").style.color = "#EF7272";
     document.getElementById("loginStatus").textContent = "아이디 또는 비밀번호가 틀렸습니다.";
